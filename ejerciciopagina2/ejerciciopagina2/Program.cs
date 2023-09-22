650e55250454d01eadace714c76d9ffe71c498d9
Gisella Hurtado Mosquera <ghurtadomosquera504@gmail.com>
2023-09-22 00:33:05 -0500

    ejercicio completado

---
+++ ejerciciopagina2/ejerciciopagina2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Desarrollar un programa que muestre la tabla de multiplicar de un numero ingresado por el teclado del 1 al 15 
+
+public class jercicioPagina2
+{
+    static void Main(string[] args)
+    {
+        int n;
+        string linea;
+
+        Console.Write("ingrese multiplicador:");
+
+        linea = Console.ReadLine();
+
+        n = int.Parse(linea);
+        for (int i = 0; i <= n; i++)
+        {
+            Console.Write(i + " x " + n + " = " + i * n +"\n" );
+
+
+        }
+
+        Console.ReadKey();
+  
+
+
+
+    }
+
+}
