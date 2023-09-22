186fccbf66c66807dfd5d55978a3b4ddd53ba120
Gisella Hurtado Mosquera <ghurtadomosquera504@gmail.com>
2023-09-21 23:55:32 -0500

    avanzar con el ejercicio

--- ejerciciosPagina/ejerciciosPagina/Program.cs
+++ ejerciciosPagina/ejerciciosPagina/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Pedir por consola un nombre de persona y el nombre de una ciudad (no hace falta que sean reales o comprobarlos) y mostrar por pantalla, el siguiente mensaje «Hola » <nombre> » bienvenido a » <ciudad>
+
+public class Ejercicio1
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("ingrese el nombre de la persona:");
+
+        string nombre = Console.ReadLine();
+
+        Console.Write("ingrese la ciudad:");
+
+        string ciudad = Console.ReadLine();
+
+        Console.WriteLine("Hola"    + nombre +     "Bienvenido a "  + ciudad);
+
+        Console.ReadLine();
+        
+    }
+    
+
+}
